76256b7c4b705b8eab7a40199dcc0aeedbcb7bce
Ramkumar Padmaraj <ramkumar.padmaraj@gmail.com>
2022-03-17 18:04:22 -0500

    Initial Commit

---
+++ com.ram.javaProject/src/main/java/com/ram/javaProject/Application.java
@@ -0,0 +1,11 @@
+package com.ram.javaProject;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Project");
+
+	}
+
+}
