99bd10aaab813843d065b6672ce93603febdb3bc
Ramkumar Padmaraj <ramkumar.padmaraj@gmail.com>
2022-03-25 19:38:41 -0500

    Added new program for Finding occurrence.

---
+++ com.ram.javaProject/src/main/java/com/ram/javaProject/FindOccurrence.java
@@ -0,0 +1,46 @@
+package com.ram.javaProject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class FindOccurrence {
+	
+	private HashMap<Character,Integer> occurence =new HashMap<Character,Integer>();
+	
+
+	public HashMap<Character,Integer> findOccurence(String str)
+	{
+		char [] ch= str.toCharArray();
+		for(int i=0;i<str.length();i++)
+		{
+			if(occurence.containsKey(ch[i]))
+			{
+			int temp=occurence.get(ch[i])+1;
+			occurence.put(ch[i], temp);
+			}
+			else
+				occurence.put(ch[i], 1);
+			}
+						
+		Iterator<Map.Entry<Character, Integer> > iterator = occurence.entrySet().iterator();
+		Integer valueToBeRemoved=1;
+    // Iterate over the HashMap
+    while (iterator.hasNext()) {
+
+        // Get the entry at this iteration
+        Map.Entry<Character, Integer> entry = iterator.next();
+
+        // Check if this value is the required value
+        if (valueToBeRemoved.equals(entry.getValue())) {
+
+            // Remove this entry from HashMap
+            iterator.remove();
+        }
+    }
+		
+		return occurence;
+		
+		
+	}
+}
